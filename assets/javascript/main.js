62850e2e4dece3775f8ffc09c38cb98b0b92868d
maylique <supercoder@AirvonSperCoder.fritz.box>
2023-12-27 17:00:44 +0100

    css edit

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -7,37 +7,42 @@ const pal = document.querySelector('#pal')
 const tee = document.querySelector('#tee')
 const form = document.querySelector('form')
 
+let bmrSum = null
+
 const bmrCalc = () => {
     if (sex.value == 'male') {
         sum = 66.5 + (13.75 * weight.value) + (5.003 * height.value) - (6.75 * age.value)
-        bmr.value = Math.round(sum)
+        bmr.value = Math.round(sum) + ' kcal'
+        return bmrSum = sum
     } else {
         sum = 655.1 + (9.563 * weight.value) + (1.850 * height.value) - (4.676 * age.value)
-        bmr.value = Math.round(sum)
+        bmr.value = Math.round(sum) + ' kcal'
+        return bmrSum = sum
     }
 }
 
 const teeCalc = () => {
     if (pal.value == 'no') {
-        sum = bmr.value * 1.2
-        tee.value = Math.round(sum)
+        sum = bmrSum * 1.2
+        tee.value = Math.round(sum) + ' kcal'
     }
     else if (pal.value == 'light') {
-        sum = bmr.value * 1.375
-        tee.value = Math.round(sum)
+        sum = bmrSum * 1.375
+        tee.value = Math.round(sum) + ' kcal'
     }
     else if (pal.value == 'moderate') {
-        sum = bmr.value * 1.55
-        tee.value = Math.round(sum)
+        sum = bmrSum * 1.55
+        tee.value = Math.round(sum) + ' kcal'
     }
     else if (pal.value == 'hard') {
-        sum = bmr.value * 1.725
-        tee.value = Math.round(sum)
+        sum = bmrSum * 1.725
+        tee.value = Math.round(sum) + ' kcal'
     }
     else if (pal.value == 'pro') {
-        sum = bmr.value * 1.9
-        tee.value = Math.round(sum)
+        sum = bmrSum * 1.9
+        tee.value = Math.round(sum) + ' kcal'
     }
+
 }
 
 form.addEventListener('input', bmrCalc)
